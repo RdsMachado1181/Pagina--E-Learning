51af36ff2b6610c4e05eed205ca5fe0b27f6b892
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2022-10-14 22:45:31 -0300

    atualização
    css, js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById( ' icones-container box' ).onmouseover = function()  {  
+    MouseOn()};
+
+
+function MouseOn(){
+    document.getElementById(  ' icones-container  box').style.backgroundColor = "blue" ;  
+}
+
+ 
