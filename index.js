93800a2a843e7392958907a29413dcd1eb4f2d01
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2022-10-26 14:59:14 -0300

    inclusão
    final

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-
-
-
-// configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-const mouseClickBotao = () =>{
-    if(  input[type='radio'] == 'checked'  ){
-       document.getElementById(  'label').style.color='blue';
-    }
  
-}
-  
-
-// configurando do botao de click
-const mouseClickBotao1 = () =>{
-    
-  
- }
-        
      
   
 
