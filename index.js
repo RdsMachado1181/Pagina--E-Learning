9f3e2fe26ff446bec34b2b74748d6efbb92cae20
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2022-10-26 14:51:39 -0300

    inclusao estilização
    estilo de passar o mouse e clicar

--- index.js
+++ index.js
@@ -2,34 +2,19 @@
 
 
 // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-const mouseOver1 = () =>{
-    label.style.backgroundColor ="blue";
+const mouseClickBotao = () =>{
+    if(  input[type='radio'] == 'checked'  ){
+       document.getElementById(  'label').style.color='blue';
+    }
  
 }
-    // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-    const mouseOut1= () =>{
+  
+
+// configurando do botao de click
+const mouseClickBotao1 = () =>{
     
-        if(  document.querySelectorAll( "label") ){
-        document.getElementById ( 'btlabel').style.backgroundColor='hsl(216, 12%, 8%)' ; 
-        
-        }   
-    } 
- // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-const mouseOver2 = () =>{
-    if(  document.querySelectorAll( "label") ){
-     
-        document.getElementById ( 'btlabel').style.backgroundColor='hsl(216, 12%, 8%)' ; 
-            
-    }
-}
-    // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-    const mouseOut2= () =>{
-    
-    if(  document.querySelectorAll( "span" ) ){
-        document.getElementById ( 't2').style.backgroundColor='hsl(216, 12%, 8%)' ; 
-            } 
-        
-    } 
+  
+ }
         
      
   
