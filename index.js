96ef7dfc42acd653d81c0a1b61ded42debe5e050
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2022-10-25 22:17:15 -0300

    inclusão estilo
    transição quando selecta o item

--- index.js
+++ index.js
@@ -2,81 +2,37 @@
 
 
 // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOver1(){    
-        document.getElementById(   'box1'  ).style.backgroundColor=' hsl(213, 4%, 47%)'; 
-    
-    } 
- // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima    
-function mouseOut1(){
-    document.getElementById( 'box1' ).style.backgroundColor='hsl(216, 12%, 8%)';
+const mouseOver1 = () =>{
+    label.style.backgroundColor ="blue";
+ 
 }
-// configurando do botao 2 a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOver2(){
-    document.getElementById(   'box2'  ).style.backgroundColor='  hsl(213, 4%, 47%)'; 
+    // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
+    const mouseOut1= () =>{
+    
+        if(  document.querySelectorAll( "label") ){
+        document.getElementById ( 'btlabel').style.backgroundColor='hsl(216, 12%, 8%)' ; 
+        
+        }   
+    } 
+ // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
+const mouseOver2 = () =>{
+    if(  document.querySelectorAll( "label") ){
+     
+        document.getElementById ( 'btlabel').style.backgroundColor='hsl(216, 12%, 8%)' ; 
+            
     }
-    function mouseOut2(){
-        document.getElementById( 'box2' ).style.backgroundColor='hsl(216, 12%, 8%)';
-    }
-// configurando do botao 3 a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOver3(){
-    document.getElementById(   'box3'  ).style.backgroundColor='  hsl(213, 4%, 47%)'; 
-    }
-    function mouseOut3(){
-        document.getElementById( 'box3' ).style.backgroundColor='hsl(216, 12%, 8%)';
-    }
-
- // configurando do botao 4 a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOver4(){
-    document.getElementById(   'box4'  ).style.backgroundColor='  hsl(213, 4%, 47%)'; 
-    }
-    function mouseOut4(){
-        document.getElementById( 'box4' ).style.backgroundColor='hsl(216, 12%, 8%)';
-    }
-// configurando do botao 5 a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOver5(){
-    document.getElementById(   'box5'  ).style.backgroundColor=' hsl(213, 4%, 47%)'; 
-    }
-    function mouseOut5(){
-        document.getElementById( 'box5' ).style.backgroundColor='hsl(216, 12%, 8%)';
-    }
-
- // configurando do botao SUMIT a alteração do background.color quando passa  e retira o mouse por cima 
-function mouseOverBotao(){
-    document.getElementById(   'botao'  ).style.backgroundColor=' white'; 
-    document.getElementById(   'botao'  ).style.color=' orange'; 
-    }
-    function mouseOutBotao(){
-        document.getElementById( 'botao' ).style.backgroundColor= ' orange';
-        document.getElementById(   'botao'  ).style.color=' white';
-       
-    }
-
-     // configurando do botao BT1 quando clicar alterando o estilo
-function mouseClickBt1(){
-    document.getElementById(   'box1' ).style.backgroundColor=' orange'; 
+}
+    // configurando do botao 1 a alteração do background.color quando passa  e retira o mouse por cima 
+    const mouseOut2= () =>{
+    
+    if(  document.querySelectorAll( "span" ) ){
+        document.getElementById ( 't2').style.backgroundColor='hsl(216, 12%, 8%)' ; 
+            } 
+        
+    } 
+        
+     
   
-    } 
-   // configurando do botao BT2 quando clicar alterando o estilo
-   function mouseClickBt2(){
-    document.getElementById(   'box2' ).style.backgroundColor=' orange';     
-    } 
-      // configurando do botao BT3 quando clicar alterando o estilo
-   function mouseClickBt3(){
-    document.getElementById(   'box3' ).style.backgroundColor=' orange';     
-    } 
-       // configurando do botao BT4 quando clicar alterando o estilo
-   function mouseClickBt4(){
-    document.getElementById(   'box4' ).style.backgroundColor=' orange';     
-    } 
-        // configurando do botao BT5 quando clicar alterando o estilo
-   function mouseClickBt5(){
-    document.getElementById(   'box5' ).style.backgroundColor=' orange';     
-    } 
 
- // configurando do botao SUMIT quando clicar alterando o estilo
-function mouseClickBotao(){
-    document.getElementById(   'botao'  ).style.backgroundColor=' orange'; 
-    document.getElementById(   'botao'  ).style.color=' gray'; 
-    } 
 
-           
+ 
